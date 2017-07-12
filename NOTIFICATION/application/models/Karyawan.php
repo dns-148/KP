890a5dea463580af342892f96fe3f48d8ced3bf8
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2017-07-12 11:28:10 +0700

    update birthday app

---
+++ NOTIFICATION/application/models/Karyawan.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Karyawan extends CI_Model {
+
+  public function cek_karyawan($table,$where)
+  {
+    return $this->db->get_where($table,$where);
+  }
+
+  public function getBirthdayRequest() {
+    //$cek="select Tanggal_lahir from karyawan where id_karyawan=1";
+    //echo $cek;
+    $querylineCount = "SELECT * FROM karyawan WHERE TO_CHAR(Tanggal_lahir,'MM-DD')=TO_CHAR(sysdate(), 'MM-DD')";
+    $query = $this->db->query($querylineCount);
+    return $query->num_rows();
+    }
+}
