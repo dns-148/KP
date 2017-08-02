35e124d980295d680387b91a1956764f1b8c6762
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2017-08-02 22:24:31 +0700

    update notification v2

--- notification_v2/application/config/config.php
+++ notification_v2/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/KP/NOTIFICATION';
+$config['base_url'] = 'http://localhost/KP/notification_v2';
 
 /*
 |--------------------------------------------------------------------------
